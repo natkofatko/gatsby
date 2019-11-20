907affc778041ca1cb6d599a094b3fc9246fbbe8
Natalia Lal <nlal@ksmpartners.com>
2019-11-20 14:58:04 -0500

    Add hookk, props and children to functional components

--- gatsby-starter-hello-world/src/pages/index.js
+++ gatsby-starter-hello-world/src/pages/index.js
@@ -1,3 +1,32 @@
 import React from "react"
+import Title from "./../components/title"
+import Effect from "./../components/effect"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+const IMAGE = "https://source.unsplash.com/random/400x200"
+export default () => {
+  return (
+    <div
+      style={{
+        color: "pink",
+
+        marginTop: 20,
+        fontSize: "33px",
+      }}
+    >
+      <Header text="Just created">
+        <Effect></Effect> <button>Hello</button>
+      </Header>
+
+      <h1>Hello Gatsby</h1>
+      <h2>What a World</h2>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <img src={IMAGE} alt=""></img>
+
+        <img src={IMAGE} alt=""></img>
+        <img src={IMAGE} alt=""></img>
+      </div>
+      <Title></Title>
+    </div>
+  )
+}
