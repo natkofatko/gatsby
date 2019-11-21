e6068b0317f95ba0abb772341dd937ac43aec02f
Natalia Lal <nlal@ksmpartners.com>
2019-11-21 08:36:44 -0500

    Add more poges and components

---
+++ gatsby-starter-hello-world/src/pages/about.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react"
+import Container from "../components/container"
+
+const User = props => (
+  <div className="user">
+    <img src={props.avatar} className="avatar" alt="" />
+    <div className="description">
+      <h2 className="username">{props.username}</h2>
+      <p className="excerpt">{props.excerpt}</p>
+    </div>
+  </div>
+)
+export default () => (
+  <Container>
+    <h1>About CSS Modules</h1>
+    <p>CSS Modules are cool</p>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+  </Container>
+)
---
+++ gatsby-starter-hello-world/src/pages/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import Layout from "../components/layout"
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>Hello</h1>
+    </Layout>
+  )
+}
+
+export default Home
